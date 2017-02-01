30e85cc7da5ce120247870e6fdb5daeb7e2d15ed
kts135 <kts135@mail.usask.ca>
2017-01-31 19:03:35 -0600

    Some very poor dragging of monitors

--- Luxsonic Project/Assets/Scripts/Display.cs
+++ Luxsonic Project/Assets/Scripts/Display.cs
@@ -1,16 +1,30 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class Display : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+public class Display : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+
+    public static GameObject itemBeingDragged;
+    Vector3 startPosition;
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        itemBeingDragged = gameObject;
+        startPosition = transform.position;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = Input.mousePosition;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        itemBeingDragged = null;
+        transform.position = startPosition;
+    }
 }
