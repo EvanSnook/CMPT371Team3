5d4df157bc2aa824bddeacc05a25a7ec10189d20
kts135 <kts135@mail.usask.ca>
2017-02-02 15:36:21 -0600

    Got displays to drag nicely, controls are weird with mouse

--- Luxsonic Project/Assets/Scripts/Display.cs
+++ Luxsonic Project/Assets/Scripts/Display.cs
@@ -4,27 +4,45 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-
+/// <summary>
+/// Class to manage display objects
+/// </summary>
 public class Display : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
 
-    public static GameObject itemBeingDragged;
-    Vector3 startPosition;
+    Vector3 screenPoint;
+    Vector3 offset;
 
+    /// <summary>
+    /// When the mouse pointer is clicked on a display object, this is used to determine the real world
+    /// point where the object was selected.
+    /// </summary>
+    /// <param name="eventData"></param>
     public void OnBeginDrag(PointerEventData eventData)
     {
-        itemBeingDragged = gameObject;
-        startPosition = transform.position;
+        // Convert the world point of the position of the display to a screen point
+        screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position); 
+        // Caclulate the offset from the clicked point to the screen point  
+        offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
     }
 
+    /// <summary>
+    /// When the object is being dragged by the mouse, update its position.
+    /// </summary>
+    /// <param name="eventData"></param>
     public void OnDrag(PointerEventData eventData)
     {
-        transform.position = Input.mousePosition;
+        // Current screen point is where the mouse is on the screen
+        Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+        // Current position is the the world point of the mouse cursor
+        Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
+        // Set the object's position to the world point of the mouse
+        transform.position = curPosition;
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
-        itemBeingDragged = null;
-        transform.position = startPosition;
+
+        //transform.position = startPosition;
     }
 }
