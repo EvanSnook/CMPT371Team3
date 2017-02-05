3b1122982c82ef2ec2cf108feab6ad75aa1b1760
Mackenzie <mkp003@mail.usask.ca>
2017-02-04 18:03:58 -0600

    From the array of files, we can take one file and convert it into an array of bytes and then convert that into a Texture2D object for use in the Display

--- Luxsonic Project/Assets/Scripts/MainMenu.cs
+++ Luxsonic Project/Assets/Scripts/MainMenu.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.Assertions;
 using System.IO;
 
@@ -8,11 +9,12 @@ public class MainMenu : MonoBehaviour
 {
 
     bool displayGUI;
-
+    SpriteRenderer testing1;//For testing DELETE
     // Use this for initialization
     void Start()
     {
         displayGUI = true;
+        testing1 = GetComponentInChildren<SpriteRenderer>();//For testing DELETE
     }
 
     // Update is called once per frame
@@ -36,12 +38,26 @@ public class MainMenu : MonoBehaviour
             //Start button should download the DICOM files from the specified directory
             if (GUI.Button(new Rect(12.5f, buttonPosY1, buttonWidth, buttonLength), "Start"))
             {
-                string path = @"C:\Users\Mackenzie\Desktop\pixelspacingtestimages\DISCIMG\IMAGES";
+                //Working on searching all files for a universal DICOM file with pngs
+                //string[] paths = Directory.GetDirectories(@"C:\", "D*");
+                //print(paths.Length);
+                //Assert.Equals(paths.Length, 1);
+
+                string path = @"C:\Users\Mackenzie\Desktop\DICOMImages";
+                
                 DirectoryInfo directory = new DirectoryInfo(path);
                 FileInfo[] info = directory.GetFiles();
-                //The file folder containing the DICOM files must not be empty!
-                Assert.IsNotNull(info);
-                print(info.Length);
+
+                //The files should be present so we will assert this
+                Assert.IsNotNull(info, "The array of files is null, but should contain files");
+                Assert.AreNotEqual<int>(0, info.Length);
+
+                byte[] dicomImages = File.ReadAllBytes(info[0].ToString());
+                Texture2D testpic = new Texture2D(10, 10);
+               // testpic.LoadImage(dicomImages);
+                
+                //print(info.Length);
+                //testing1.sprite = Sprite.Create(testpic, new Rect(0, 0, testpic.width, testpic.height), new Vector2(0.5f, 0.5f));
                 displayGUI = false;
             }
             //Quit button should simply exit the application
