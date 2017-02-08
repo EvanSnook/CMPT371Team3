2c7bce06277dcfa55a885ceda978b58a8c76e258
Evan <evan.snook@usask.ca>
2017-02-08 01:10:04 -0600

    deleted examples and rename unitTests to tests

---
+++ Luxsonic Project/Assets/UnityTestTools/Tests/Editor/SmokeTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using NUnit.Framework;
+using UnityEngine;
+
+namespace UnityTest
+{
+    [TestFixture]
+    [Category("Sample Tests")]
+    internal class SampleTests
+    {
+        /*
+        [Test]
+        [Category("Failing Tests")]
+        public void FailingTest()
+        {
+            Assert.Fail();
+        }
+        */
+
+        [Test]
+        public void PassingTest()
+        {
+            Assert.Pass();
+        }
+    }
+}
