c0103878822c0bb42eef466018bad9634989960a
Evan <evan.snook@usask.ca>
2017-02-07 23:32:43 -0600

    a single unit test to assert a pass
    also commented out deprecated code and failing tests in unity test tools
    examples

---
+++ Luxsonic Project/Assets/UnityTestTools/UnitTesting/Editor/BrightnessSliderTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using NUnit.Framework;
+using UnityEngine;
+
+namespace UnityTest
+{
+    [TestFixture]
+    [Category("Sample Tests")]
+    internal class SampleTests
+    {
+        [Test]
+        public void PassingTest()
+        {
+            Assert.Pass();
+        }
+    }
+}
