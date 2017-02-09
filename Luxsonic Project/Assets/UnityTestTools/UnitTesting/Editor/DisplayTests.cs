c79226bd2ba46be1708cc9e037db92fe8b6467a2
kts135 <kts135@mail.usask.ca>
2017-02-09 16:14:15 -0600

    Some attempt at running tests

--- Luxsonic Project/Assets/UnityTestTools/UnitTesting/Editor/DisplayTests.cs
+++ Luxsonic Project/Assets/UnityTestTools/UnitTesting/Editor/DisplayTests.cs
@@ -3,13 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using NUnit.Framework;
 
-namespace UnityTest
-{
     [TestFixture]
     [Category("Unit Tests")]
-    internal class DisplayTests
+    public class DisplayTests
     {
-        
+        [Test]
+        public void TestConstructor()
+        {
+            Texture2D tex = new Texture2D(100, 100);    // Create a texture to give to the display
+
+            Display disp = new Display(tex);    // Create a new display
+
+            Assert.IsNotNull(disp.imageRenderer);   // Make sure the image renderer was set properly
+            Assert.IsNotNull(disp.myTransform);     // As well as the transform object
+        }
     }
-}   
+   
 
