ee65b0512193708637a9a85cdafd51ea29b70055
kts135 <kts135@mail.usask.ca>
2017-02-11 14:06:33 -0600

    Added a load button

---
+++ Luxsonic Project/Assets/Scripts/WorkspaceManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorkspaceManager : MonoBehaviour {
+
+    [SerializeField]
+    private float buttonWidth = 100;
+    [SerializeField]
+    private float buttonHeight = 50;
+
+    public Transform myTransform;
+
+    public void Start()
+    {
+        this.myTransform = this.GetComponent<Transform>();
+    }
+
+    public void OnGUI()
+    {
+        // Convert the world point of the display to a screen point
+        Vector3 displayScreenPoint = Camera.main.WorldToScreenPoint(myTransform.position);
+
+        // The positions of the buttons, relative to the calculated screen point of the display.
+        Vector3 loadButtonPosition = displayScreenPoint - new Vector3(0, 0, 0);
+
+        // The loadbutton
+        if (GUI.Button(new Rect(loadButtonPosition.x, Screen.height - loadButtonPosition.y, buttonWidth, buttonHeight), "Load Image"))
+        {
+            // Bring up the load screen
+        }
+    }
+}
