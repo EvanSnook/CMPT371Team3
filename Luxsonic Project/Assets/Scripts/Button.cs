b04d6ee0a254d16b0a7e560bac5aec0ae59ecbb7
Heramb Sawant <heramb.sawant@usask.ca>
2017-02-11 17:50:24 -0600

    added a test button to test collider functionality

---
+++ Luxsonic Project/Assets/Scripts/Button.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour {
+
+    public string name;
+    public GameObject manager;
+    public TextMesh textObject;
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        manager.SendMessage("buttonClicked", name);
+        textObject.color = Color.black;
+    }
+    // Use this for initialization
+    void Start () {
+        textObject = gameObject.GetComponentInChildren<TextMesh>();
+        textObject.text = name;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Luxsonic Project/Assets/Scripts/WorkspaceManager.cs
+++ Luxsonic Project/Assets/Scripts/WorkspaceManager.cs
@@ -12,24 +12,36 @@ public class WorkspaceManager : MonoBehaviour {
     public GameObject loadBar;
 
     public Transform myTransform;
+    public Button button;
 
     public void Start()
     {
         this.myTransform = this.GetComponent<Transform>();
+        DisplayMenu();
     }
 
-    public void OnGUI()
+    public void DisplayMenu(){
+        Button loadButton = Instantiate(button, new Vector3(0,0,0), new Quaternion(0, 0, 0, 0));
+        loadButton.name = "load";
+    }
+    public void ButtonClicked(string name)
     {
-        // Convert the world point of the display to a screen point
-        Vector3 displayScreenPoint = Camera.main.WorldToScreenPoint(myTransform.position);
-
-        // The positions of the buttons, relative to the calculated screen point of the display.
-        Vector3 loadButtonPosition = displayScreenPoint - new Vector3(0, 0, 0);
-
-        // The loadbutton
-        if (GUI.Button(new Rect(loadButtonPosition.x, Screen.height - loadButtonPosition.y, buttonWidth, buttonHeight), "Load Image"))
+        if(name == "load")
         {
             Instantiate(loadBar, new Vector3(0, 0, 0), new Quaternion(0, 0, 0, 0));
         }
-    }
+    } 
+    //public void OnGUI()
+    //{
+    //    // Convert the world point of the display to a screen point
+    //    Vector3 displayScreenPoint = Camera.main.WorldToScreenPoint(myTransform.position);
+
+    //    // The positions of the buttons, relative to the calculated screen point of the display.
+    //    Vector3 loadButtonPosition = displayScreenPoint - new Vector3(0, 0, 0);
+
+    //    // The loadbutton
+    //    if (GUI.Button(new Rect(loadButtonPosition.x, Screen.height - loadButtonPosition.y, buttonWidth, buttonHeight), "Load Image"))
+    //    {
+    //    }
+    //}
 }
