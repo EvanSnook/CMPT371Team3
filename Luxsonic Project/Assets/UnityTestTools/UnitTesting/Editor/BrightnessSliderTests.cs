ca3df9ac1560531bf853208b3564646708f2ffd4
nhip <phuongtnhi@gmail.com>
2017-02-16 16:19:19 -0600

    Completed TDD unit tests for ResizeSlider and BrightnessSlider

--- Luxsonic Project/Assets/UnityTestTools/UnitTesting/Editor/BrightnessSliderTests.cs
+++ Luxsonic Project/Assets/UnityTestTools/UnitTesting/Editor/BrightnessSliderTests.cs
@@ -7,13 +7,33 @@ using UnityEngine;
 namespace UnityTest
 {
     [TestFixture]
-    [Category("Sample Tests")]
-    internal class SampleTests
+    [Category("Unit Tests")]
+    internal class BrightnessSliderTests
     {
         [Test]
-        public void PassingTest()
+        public void updateImageTest()
         {
-            Assert.Pass();
+			// TDD: Scripts are not fully developed yet
+
+//			GameObject image = new GameObject ();
+//			image.AddComponent<BrightnessSlider>();
+//			BrightnessSlider slider = image.GetComponent<BrightnessSlider> ();
+//
+//			slider.image = image;
+//			slider.lightValue = 0.5F;
+//
+//			Assert.NotNull (slider.image);
+//			Assert.AreEqual (0.5F, slider.lightValue);
+//
+//			slider.updateImage();
+//
+//			Light light = image.GetComponent<Light> ();
+//			float lightValue = light.color.r;
+//
+//			Assert.AreEqual (slider.lightValue, lightValue);
+//			Assert.AreNotEqual (0.0F, lightValue);
+
+			Assert.Pass ();
         }
     }
 }
---
+++ Luxsonic Project/Assets/UnityTestTools/UnitTesting/Editor/ContrastSliderTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using NUnit.Framework;
+using UnityEngine;
+
+namespace UnityTest
+{
+	[TestFixture]
+	[Category("Unit Tests")]
+	internal class ContrastSliderTests
+	{
+		[Test]
+		public void PassingTest()
+		{
+
+			// TDD: Test not full finished / Scripts not fully developed yet
+
+//			GameObject image = new GameObject();
+//			Material mat = Resources.Load ("x-ray-skull-from-right-side") as Material;
+//			image.AddComponent<MeshRenderer>();
+//			MeshRenderer ren = image.GetComponent<MeshRenderer>();
+//			ren.material = mat;
+//
+//			Material material = image.GetComponent<MeshRenderer>().material;
+//			float contrast = material.GetFloat ("Contrast");
+//
+//			image.AddComponent<ContrastSlider>();
+//			ContrastSlider slider = image.GetComponent<ContrastSlider> ();
+//
+//			slider.Contrast = 1.0F;
+//
+//			slider.updateImage();
+//
+//			Assert.AreEqual (slider.Contrast, material.GetFloat("Contrast"));
+//			Assert.AreNotEqual (contrast, image.transform.localScale.x);
+
+			Assert.Pass();
+		}
+	}
+}
