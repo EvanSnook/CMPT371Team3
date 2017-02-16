aa209d8b520e9adf233256900b621ad0435534da
Evan <snovan@hotmail.com>
2017-02-16 13:34:23 -0600

    Revert "Merge pull request #13 from IxLahiji/SmokeTests"
    This reverts commit 27518ea857566decf12556e2e56b2a134571d4da, reversing
    changes made to 38c43b7aac1ebf891192ce8bc067c741437c85cb.
    
    "2nd failed build revert from smoke test issues"

--- Luxsonic Project/Assets/UnityTestTools/UnitTesting/Editor/SmokeTests/SmokeTests.cs
+++ Luxsonic Project/Assets/UnityTestTools/UnitTesting/Editor/SmokeTests/SmokeTests.cs
@@ -1,85 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
-using System;
-using System.IO;
-using NUnit.Framework;
-using UnityEditor;
-using UnityEditor.SceneManagement;
 
-[TestFixture]
-public class SmokeTests {
+public class SmokeTests : MonoBehaviour {
 
-    private TestContext testContextInstance;
-
-    [Test]
-	[Category("Dependencies")]
-	public void OVRCheck(){
-		String addPath = "Assets/OVR";
-		String targetPath = Path.Combine (getAssetPath(), addPath);
-		Console.WriteLine (targetPath);
-        Assert.IsTrue(Directory.Exists(targetPath) && (Directory.GetDirectories(targetPath).Length > 0) && (Directory.GetFiles(targetPath).Length > 0));
-	}
-
-
-	[Test]
-	[Category("Dependencies")]
-	public void OVRAvatarCheck(){
-		String addPath = "Assets/OvrAvatar";
-		String targetPath = Path.Combine (getAssetPath (), addPath);
-		Console.WriteLine (targetPath);
-        Assert.IsTrue(Directory.Exists(targetPath) && (Directory.GetDirectories(targetPath).Length > 0) && (Directory.GetFiles(targetPath).Length > 0)) ;
-	}
-
-	[Test]
-	[Category("Dependencies")]
-	public void gamePadBundleCheck(){
-		String addPath = "Assets/Plugins/OVRGamepad.bundle/Contents";
-		String targetPath = Path.Combine (getAssetPath (), addPath);
-		Console.WriteLine (targetPath);
-        Assert.IsTrue(Directory.Exists(targetPath) && (Directory.GetDirectories(targetPath).Length > 0) && (Directory.GetFiles(targetPath).Length > 0)) ;
-	}
-
-	[Test]
-	[Category("Dependencies")]
-	public void OVRAvatarSettingsCheck(){
-		String addPath = "Assets/Resources";
-		String targetPath = Path.Combine (getAssetPath (), addPath);
-		Console.WriteLine (targetPath);
-        Assert.IsTrue(Directory.Exists(targetPath) && (Directory.GetFiles(targetPath).Length > 0)) ;
-	}
-
-	[Test]
-	[Category("Dependencies")]
-	public void imageEffectsCheck(){
-		String addPath = "Assets/Standard Assets/Editor/ImageEffects";
-		String targetPath = Path.Combine (getAssetPath (), addPath);
-		Console.WriteLine (targetPath);
-        Assert.IsTrue(Directory.Exists(targetPath) && (Directory.GetFiles(targetPath).Length > 0));
-	}
-
-	[Test]
-	[Category("Dependencies")]
-	public void EffectsCheck(){
-		String addPath = "Assets/Standard Assets/Effects";
-		String targetPath = Path.Combine (getAssetPath (), addPath);
-		Console.WriteLine (targetPath);
-        Assert.IsTrue(Directory.Exists(targetPath) && (Directory.GetDirectories(targetPath).Length > 0) && (Directory.GetFiles(targetPath).Length > 0)) ;
-	}
+	// Use this for initialization
+	void Start () {
 		
-	private String getAssetPath(){
-		String absPath = Directory.GetCurrentDirectory ();
-		String newString = Directory.GetParent(absPath).FullName;
-		newString = Directory.GetParent(newString).FullName;
-		newString = Directory.GetParent(newString).FullName;
-		return newString;
 	}
-
-	[Test]
-	[Category("Scenes")]
-	public void defaultSceneCheck(){
-        Assert.IsTrue(EditorSceneManager.GetSceneByBuildIndex(0).IsValid()) ;
+	
+	// Update is called once per frame
+	void Update () {
+		
 	}
-	 
 }
