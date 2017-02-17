602ef37689b95acb2bf0b86cf442bedd97a56d4c
Mackenzie <mkp003@mail.usask.ca>
2017-02-16 19:34:35 -0600

    Adding material from ID2_Load

---
+++ Luxsonic Project/LoadBar/DirectoryButton.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DirectoryButton : MonoBehaviour {
+
+    GameObject parent;
+    string currentDirectory;
+    string[] listOfCurrentDirectories;
+    string[] listOfCurrentFiles;
+
+    DirectoryButton(string path)
+    {
+        currentDirectory = path;
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnCollisionEnter(Collision hit)
+    {
+        parent.SendMessage("EnterDirectory", currentDirectory);
+    }
+}
