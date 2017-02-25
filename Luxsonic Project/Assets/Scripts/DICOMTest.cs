ef8d0f325fe39a0bf3d28ed7668eecc5e9cefeff
J Hindman <idontwanttotellyoumyemail@goaway.com>
2017-02-25 17:50:25 -0600

    Incorporated Fellow Oak DICOM library and ran a few tests.

---
+++ Luxsonic Project/Assets/Scripts/DICOMTest.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Dicom;
+using Dicom.Imaging;
+using Dicom.Log;
+
+public class DICOMTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log("Starting test.");
+		Dicom.DicomFile obj = Dicom.DicomFile.Open("Assets/Resources/DICOM/CR-MONO1-10-chest");
+		var PatientName = obj.Dataset.Get< string>(Dicom.DicomTag.PatientName, null );
+		Debug.Log("" + PatientName);
+		Debug.Log("" + obj.Dataset.Get<string>(Dicom.DicomTag.PhotometricInterpretation));
+		var image = new DicomImage(obj.Dataset);
+		var texture = image.RenderImage().AsTexture2D();
+		gameObject.GetComponent<GUITexture>().texture = texture;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
