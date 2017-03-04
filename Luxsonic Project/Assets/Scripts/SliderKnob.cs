37cabc13a498a3df9c1f88ca60dfe7f03ca16bf4
nhip <phuongtnhi@gmail.com>
2017-03-03 20:24:39 -0600

    Change the functionaly of the Slider Knob that it should stay within its limits when being moved

--- Luxsonic Project/Assets/Scripts/SliderKnob.cs
+++ Luxsonic Project/Assets/Scripts/SliderKnob.cs
@@ -7,8 +7,16 @@ public class SliderKnob : MonoBehaviour {
     private Vector3 leftBoundary;
     private Vector3 rightBoundary;
 
+	private float yCoord;
+	private float zCoord;
+
     public GameObject knob;
     public GameObject sliderBase;
+
+	void Start() {
+		yCoord = this.transform.position.y;
+		zCoord = this.transform.position.z;
+	}
 
     public void SetLeftBoundary(Vector3 bound)
     {
@@ -21,22 +29,32 @@ public class SliderKnob : MonoBehaviour {
         this.rightBoundary = bound;
     }
 
+	/// <summary>
+	/// Gets the left boundary.
+	/// </summary>
+	/// <returns>The left boundary.</returns>
     public Vector3 getLeftBoundary()
     {
         return this.leftBoundary;
     }
 
+	/// <summary>
+	/// Gets the right boundary.
+	/// </summary>
+	/// <returns>The right boundary.</returns>
     public Vector3 getRightBoundary()
     {
         return this.rightBoundary;
     }
 
+	public void FixedUpdate() {
+		knob.transform.position = new Vector3 (Mathf.Clamp(this.transform.position.x, leftBoundary.x, rightBoundary.x), yCoord, zCoord);
+		sliderBase.GetComponent<SliderBar>().setUpdate(true);
+	}
+
     void OnMouseDrag()
     {
-        if (Input.mousePosition.x <= this.rightBoundary.x && Input.mousePosition.x >= this.leftBoundary.x)
-        {
-            knob.transform.position = new Vector3(Input.mousePosition.x, knob.transform.position.y, knob.transform.position.z);
-        }
+		knob.transform.position = new Vector3(Mathf.Clamp(Input.mousePosition.x, leftBoundary.x, rightBoundary.x), knob.transform.position.y, knob.transform.position.z);
         sliderBase.GetComponent<SliderBar>().setUpdate(true);
     }
 
