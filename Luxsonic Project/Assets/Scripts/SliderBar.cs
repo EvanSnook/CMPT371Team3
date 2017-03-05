a95509f9bd2eb4cffea066d2c5f1355de83f1d56
nhip <phuongtnhi@gmail.com>
2017-03-05 16:20:34 -0600

    Updated the LoadBar, SliderBar, SliderKnob, ThumbNail, and TouchAndGrab script comments and variable names (excluding TouchAndGrab for var names), fixed prefabs for the updated variable names

--- Luxsonic Project/Assets/Scripts/SliderBar.cs
+++ Luxsonic Project/Assets/Scripts/SliderBar.cs
@@ -4,107 +4,122 @@ using UnityEngine;
 
 public class SliderBar : MonoBehaviour {
 
-    public GameObject sliderBase;
-    public GameObject sliderKnob;
+	// The object prefab for the slider base and knob
+    public GameObject sliderBasePrefab;
+    public GameObject sliderKnobPrefab;
+	// The object that the slider will be reporting too
     public GameObject manager;
 
-    Vector3 maxPosition;
-    Vector3 minPosition;
-    Vector3 knobPosition;
+	// The maximum coordinate of the slider in world space
+    Vector3 maxCoord;
+	// The minimum coordinate of the slider in world space
+    Vector3 minCoord;
+	// The coordinate of the knob in world space
+    Vector3 knobCoord;
 
-    private float convertedMaxPosition;
-	private float convertedKnobPosition;
+	// The maxCoord normalized (0 to maxScale)
+    private float maxScale;
+	// The knobCoord normalized (0 - knobScaleValue - maxScale)
+	private float knobScaleValue;
 
-    float maxVal = 1;
-    float minVal = 0;
+	// The max represented as a percent
+    float maxPercentScale = 1;
+	// the min represented as a percent
+	float minPercentScale = 0;
 
+	// Indicates 
     private bool update = false;
-
-	// Use this for initialization
-	void Start () {
-//        Renderer rend = sliderBase.GetComponent<Renderer>();
-//        maxPosition = rend.bounds.max;
-//        minPosition = rend.bounds.min;
-//		Debug.Log ("minPos: " + minPosition);
-//
-//        convertedMaxPosition = maxPosition.x - minPosition.x;
-
-//        Setup(sliderKnob.transform.position.x);
-	}
 
     /// <summary>
     /// Called by the manager to set up the slider
+	/// Pre:: currentVal is passed in from the Copy
+	/// Post:: A Slider is created for the Copy
+	/// Return:: nothing
     /// </summary>
     /// <param name="currentVal">Current Value of the image we are updating</param>
     public void Setup(float currentVal)
     {
 		// Getting bounds of the slider in the world space
-		Renderer rend = sliderBase.GetComponent<Renderer>();
-		maxPosition = rend.bounds.max;
-		minPosition = rend.bounds.min;
-		Debug.Log ("max position: " + maxPosition);
-		Debug.Log ("min position: " + minPosition);
+		Renderer rend = sliderBasePrefab.GetComponent<Renderer>();
+		maxCoord = rend.bounds.max;
+		minCoord = rend.bounds.min;
+		Debug.Log ("max position: " + maxCoord);
+		Debug.Log ("min position: " + minCoord);
 
 		// Converting world space max to scaled max
-		convertedMaxPosition = maxPosition.x - minPosition.x;
+		maxScale = maxCoord.x - minCoord.x;
 
 		// Converting the value retrieved from image
-		convertedKnobPosition = ConvertFromPercentOfSliderToScale(currentVal);
+		knobScaleValue = ConvertFromPercentOfSliderToScale(currentVal);
 
 		// Moving slider knob to position of image value
-		sliderKnob.transform.position = new Vector3(ConvertFromScaleToCoord(ConvertFromPercentOfSliderToScale(currentVal)), sliderKnob.transform.position.y, sliderKnob.transform.position.z);
+		sliderKnobPrefab.transform.position = new Vector3(ConvertFromScaleToCoord(ConvertFromPercentOfSliderToScale(currentVal)),
+			sliderKnobPrefab.transform.position.y, sliderKnobPrefab.transform.position.z);
 
 		// Updating the current position to the value of the slider knob
-        knobPosition = sliderKnob.transform.position;
-		Debug.Log ("knob: " + knobPosition);
+        knobCoord = sliderKnobPrefab.transform.position;
+		Debug.Log ("knob: " + knobCoord);
 
 		// Setting the boundaries of the knob to the slider boundaries
-        sliderKnob.GetComponent<SliderKnob>().SetLeftBoundary(this.minPosition);
-        sliderKnob.GetComponent<SliderKnob>().SetRightBoundary(this.maxPosition);
+		sliderKnobPrefab.GetComponent<SliderKnob>().SetLeftBoundary(this.minCoord);
+		sliderKnobPrefab.GetComponent<SliderKnob>().SetRightBoundary(this.maxCoord);
     }
-		
+
+	/// <summary>
+	/// Raises the collision enter event.
+	/// </summary>
+	/// <param name="collision">Collision.</param>
     void OnCollisionEnter(Collision collision)
     {
         this.update = true;
     }
 
+	/// <summary>
+	/// Raises the collision exit event.
+	/// </summary>
+	/// <param name="collision">Collision.</param>
     private void OnCollisionExit(Collision collision)
     {
         this.update = false;
     }
 
 	/// <summary>
-	/// Converts the coordinate to the scale of the slider
+	/// Converts the coordinate to the scale of the slider by taking
+	/// the value passed in a subtracting the minimum coordinate
 	/// </summary>
 	/// <returns>The scaled version of the coordinate.</returns>
 	/// <param name="value">Value.</param>
     float ConvertFromCoordToScale(float value)
     {
-        return value - minPosition.x;
+        return value - minCoord.x;
     }
 
 	/// <summary>
-	/// Convert the scale of the slider to the coordinate
+	/// Convert the scale of the slider to the coordinate by taking
+	/// the value and adding the minimum coordinate
 	/// </summary>
 	/// <returns>The coordinate of the scale.</returns>
 	/// <param name="value">Value.</param>
     float ConvertFromScaleToCoord(float value)
     {
-        return value + minPosition.x;
+        return value + minCoord.x;
     }
 
 	/// <summary>
-	/// Convert coordinate to percent scale
+	/// Convert coordinate to percent scale by taking the value and 
+	/// subtracting the minimum coordinate and then dividing by the 
+	/// max scale number to get the percent
 	/// </summary>
 	/// <returns>The percent of slider.</returns>
 	/// <param name="value">Value.</param>
 	float ConvertFromCoordToPercentOfSlider(float value)
 	{
-		return (value - minPosition.x) / convertedMaxPosition;
+		return (value - minCoord.x) / maxScale;
 	}
 
 	/// <summary>
-	/// Convert the percent of slider to the coordinate
+	/// Convert the percent of slider to the coordinate by taking the value
+	/// converting it to the scale, and then converting it to the coord
 	/// </summary>
 	/// <returns>The coordinate of the value.</returns>
 	/// <param name="value">Value.</param>
@@ -114,39 +129,47 @@ public class SliderBar : MonoBehaviour {
 	}
 
 	/// <summary>
-	/// Converts percent scale to scale
+	/// Converts percent scale to scale by taking the value
+	/// and multiplying it by the maximum scale
 	/// </summary>
 	/// <returns>The scaled value.</returns>
 	/// <param name="value">Value.</param>
 	float ConvertFromPercentOfSliderToScale(float value)
 	{
-		return value * convertedMaxPosition;
+		return value * maxScale;
 	}
 
 	/// <summary>
-	/// Converts the scale to the percent of slider
+	/// Converts the scale to the percent of slider by taking the value
+	/// and dividing it by the maximum scale
 	/// </summary>
 	/// <returns>The percent of slider.</returns>
 	/// <param name="value">Value.</param>
 	float ConvertFromScaleToPercentOfSlider(float value)
 	{
-		return value / convertedMaxPosition;
+		return value / maxScale;
 	}
 
+	/// <summary>
+	/// Sets the update.
+	/// </summary>
+	/// <param name="update">If set to <c>true</c> update.</param>
     public void setUpdate(bool update)
     {
         this.update = update;
     }
 	
-	// Update is called once per frame
+	/// <summary>
+	/// Update this instance.
+	/// </summary>
 	void Update ()
     {
+		// if update is true, then update the slider and slider knob
         if (update)
         {
-//            Debug.Log("Updating");
-            knobPosition = sliderKnob.transform.position;
-			Debug.Log ("Converted: " + ConvertFromCoordToPercentOfSlider (knobPosition.x));
-			manager.SendMessage("SliderUpdate", ConvertFromCoordToPercentOfSlider(knobPosition.x));
+			knobCoord = sliderKnobPrefab.transform.position;
+			Debug.Log ("Converted: " + ConvertFromCoordToPercentOfSlider (knobCoord.x));
+			manager.SendMessage("SliderUpdate", ConvertFromCoordToPercentOfSlider(knobCoord.x));
         }
 	}
 }
--- Luxsonic Project/Assets/Scripts/SliderKnob.cs
+++ Luxsonic Project/Assets/Scripts/SliderKnob.cs
@@ -4,26 +4,40 @@ using UnityEngine;
 
 public class SliderKnob : MonoBehaviour {
 
+	// The right and left boundary of the slider
     private Vector3 leftBoundary;
     private Vector3 rightBoundary;
 
+	// the constant y and z coordinate that the slider will stay on
 	private float yCoord;
 	private float zCoord;
 
-    public GameObject knob;
-    public GameObject sliderBase;
+	// The object prefabs for the knob and slider base
+    public GameObject knobPrefab;
+    public GameObject sliderBasePrefab;
 
+	/// <summary>
+	/// Start this instance. Store the y and z coordinates of the knob
+	/// </summary>
 	void Start() {
 		yCoord = this.transform.position.y;
 		zCoord = this.transform.position.z;
 	}
 
+	/// <summary>
+	/// Sets the left boundary.
+	/// </summary>
+	/// <param name="bound">Bound.</param>
     public void SetLeftBoundary(Vector3 bound)
     {
         this.leftBoundary = bound;
        
     }
 
+	/// <summary>
+	/// Sets the right boundary.
+	/// </summary>
+	/// <param name="bound">Bound.</param>
     public void SetRightBoundary(Vector3 bound)
     {
         this.rightBoundary = bound;
@@ -47,20 +61,31 @@ public class SliderKnob : MonoBehaviour {
         return this.rightBoundary;
     }
 
+	/// <summary>
+	/// Update the position of the knob but clam the x coordinate between the left and right boundary
+	/// </summary>
 	public void FixedUpdate() {
-		knob.transform.position = new Vector3 (Mathf.Clamp(this.transform.position.x, leftBoundary.x, rightBoundary.x), yCoord, zCoord);
-		sliderBase.GetComponent<SliderBar>().setUpdate(true);
+		knobPrefab.transform.position = new Vector3 (Mathf.Clamp(this.transform.position.x, leftBoundary.x, rightBoundary.x), 
+			yCoord, zCoord);
+		sliderBasePrefab.GetComponent<SliderBar>().setUpdate(true);
 	}
 
+	/// <summary>
+	/// Raises the mouse drag event. Update the knob according to the mouse position with the
+	/// x coord clamped between the left and right boundary
+	/// </summary>
     void OnMouseDrag()
     {
-		knob.transform.position = new Vector3(Mathf.Clamp(Input.mousePosition.x, leftBoundary.x, rightBoundary.x), knob.transform.position.y, knob.transform.position.z);
-        sliderBase.GetComponent<SliderBar>().setUpdate(true);
+		knobPrefab.transform.position = new Vector3(Mathf.Clamp(Input.mousePosition.x, leftBoundary.x, rightBoundary.x), knobPrefab.transform.position.y, knobPrefab.transform.position.z);
+        sliderBasePrefab.GetComponent<SliderBar>().setUpdate(true);
     }
 
+	/// <summary>
+	/// Raises the mouse up event. Update the setUpdate value to false
+	/// </summary>
     private void OnMouseUp()
     {
-        sliderBase.GetComponent<SliderBar>().setUpdate(false);
+        sliderBasePrefab.GetComponent<SliderBar>().setUpdate(false);
     }
 
 }
