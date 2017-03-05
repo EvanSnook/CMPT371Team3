9dae7f6b8a59cfe98570917c97e2d0da9608fde5
SavoyPixel <SavoyPixel@gmail.com>
2017-03-04 19:01:34 -0600

    Copy System Tests nearly complete, waiting on new build.
    This is the nearly completed solution to the first round of System
    Tests.
    Should work once Development pushes the build including "Copy" tag to
    the Copy prefabs.

--- Luxsonic Project/Assets/UnityTestTools/UnitTesting/Editor/SystemTests/SystemTests.cs
+++ Luxsonic Project/Assets/UnityTestTools/UnitTesting/Editor/SystemTests/SystemTests.cs
@@ -3,22 +3,54 @@ using System.Collections.Generic;
 using UnityEngine;
 using NUnit.Framework;
 
+[TestFixture]
+[Category("System Tests")]
 public class SystemTests : MonoBehaviour {
 
+	[Test]
+	public void copyExists()
+	{
+		Dashboard dashboard = new Dashboard ();
+		Display testDisplay = new Display ();
+		dashboard.display = testDisplay;
+
+		//dashboard.dissplay = GameObject.FindGameObjectWithTag("DisplayImage").GetComponent<Display>();
+
+		dashboard.VRButtonClicked ("Load"); // Load in the default image.
+		testDisplay.CreateCopy(testDisplay.GetImages()[0]);								// Creating the first copy
+
+		/* ELEGANT, LONG SOLUTION 
+			GameObject[] copies = new GameObject[1];
+			copies = GameObject.FindGameObjectsWithTag ("Copy");
+			Assert.AreEqual(copies.GetLength, 1);
+		*/
+
+		//Debug.Log (GameObject.FindGameObjectsWithTag ("Copy").GetLength);
+
+		/* ONE LINER SOL'N */
+		Assert.AreEqual(GameObject.FindGameObjectsWithTag ("Copy").GetLength, 1);		//Check that there is a copy created. 
+	}
 
 	[Test]
-	[Category("copyImage")]
-	public void OVRCheck()
+	public void copyDifferentThanDisplay()
 	{
 		Dashboard dashboard = new Dashboard ();
 		Display display = new Display ();
-		dashboard.display = GameObject.FindGameObjectWithTag("ImageManager").GetComponent<Display>();
-		dashboard.VRButtonClicked ("Load");
-		//do sprite comparison with display.images
-		Thumbnail thumbnail = (display.getThumbnails ())[0];
-		Display.createCopy (((Tray.getThumbnails ()) [0]).image);
-		Copy[] afterCopies = GameObject.FindGameObjectsWithTag("Copy");
+
+		dashboard.display = GameObject.FindGameObjectWithTag("DisplayImage").GetComponent<Display>();
+		dashboard.VRButtonClicked ("Load"); // Load in the default images.
+		display.CreateCopy(display.GetImages[0]); // Creating the first copy
+
+		//Ensure image is same.... && Game object is different... 
+		Assert (GameObject.FindGameObjectWithTag("DisplayImage").GetComponent<SpriteRenderer>().sprite == GameObject.FindGameObjectWithTag("Copy").GetComponent<SpriteRenderer>().sprite &&
+			GameObject.FindGameObjectWithTag("DisplayImage") != GameObject.FindGameObjectsWithTag("Copy)"));
 	}
 
+	[Test]
+	public void copyDifferentThanTray(){
 
+	}
 }
+
+
+//do sprite comparison with display.images4
