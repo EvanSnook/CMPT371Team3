38cc3b8dd939ca186633dda20cf0e9a7b3a8f2bf
Heramb Sawant <heramb.sawant@usask.ca>
2017-03-05 14:55:48 -0600

    Added unit tests to copy

--- Luxsonic Project/Assets/UnityTestTools/UnitTesting/Editor/CopyTests.cs
+++ Luxsonic Project/Assets/UnityTestTools/UnitTesting/Editor/CopyTests.cs
@@ -25,6 +25,8 @@ public class CopyTests
 
         // The properties of the display should not be null
         Assert.IsNotNull(newCopy.myTransform, "The transform on the new display is Null");
+        Assert.AreEqual(tex, newCopy.GetComponent<MeshRenderer>().material.GetTexture("_MainTex"),
+                            "Image in copy does not match image assigned");
     }
 
     [Test]
@@ -49,13 +51,39 @@ public class CopyTests
 
         // The size of the button list should be greater than 0
         Assert.Greater(newCopy.GetButtons().Count, 0, "The list of buttons for the display was empty");
-
+        Assert.AreEqual(newCopy.GetButtons().Count, 7, "The correct number of buttons have not been created");
         newCopy.HideButtons();
 
         // The size of the button list should be 0
         Assert.AreEqual(newCopy.GetButtons().Count, 0, "The list of buttons was not cleared properly");
     }
 
+    [Test]
+    // Test the functionality button clicks and make sure the
+    // switch cases work
+    public void TestVRButtonClicks()
+    {
+        GameObject copyObj = new GameObject();
+        copyObj.AddComponent<Copy>();
+        copyObj.AddComponent<MeshRenderer>();
+        copyObj.AddComponent<BoxCollider>();
+        Copy newCopy = copyObj.GetComponent<Copy>();
+        Texture2D tex = new Texture2D(100, 100);
+
+        GameObject buttonObj = new GameObject();
+        buttonObj.AddComponent<VRButton>();
+        newCopy.button = buttonObj.GetComponent<VRButton>();
+
+        newCopy.NewCopy(tex);
+
+        newCopy.DisplayButtons();
+
+        Assert.AreEqual(newCopy.GetButtons().Count, 7, "The correct number of buttons do not exist");
+
+        newCopy.VRButtonClicked("Close");
+
+        Assert.AreEqual(newCopy.GetButtons().Count, 0, "Close case in VRButtonClicks() failed");
+    }
 }
    
 
