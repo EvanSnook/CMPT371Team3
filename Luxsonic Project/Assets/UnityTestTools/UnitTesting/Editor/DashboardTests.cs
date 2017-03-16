c77a9545d070be5f8a49be6effe7505593b00041
Brianne Kostur <bdk511@mail.usask.ca>
2017-03-16 14:43:24 -0600

    worked on dashboard unit tests. not yet finished

--- Luxsonic Project/Assets/UnityTestTools/UnitTesting/Editor/DashboardTests.cs
+++ Luxsonic Project/Assets/UnityTestTools/UnitTesting/Editor/DashboardTests.cs
@@ -10,7 +10,23 @@ public class DashboardTests {
     [Test]
     public void DisplayMenu()
     {
-        // Funtionality not complete yet
+		/*GameObject dashObj = new GameObject();
+		dashObj.AddComponent<Dashboard>();
+		Dashboard dash = dashObj.GetComponent<Dashboard>();
+
+
+		dash.planePrefab = new GameObject();
+
+		GameObject dispObj = new GameObject();
+		dispObj.AddComponent<Display>();
+
+		GameObject buttonObj = new GameObject();
+		buttonObj.AddComponent<VRButton> ();
+		dash.button = buttonObj.GetComponent<VRButton>();
+
+		dash.Start ();
+*/
+
         Assert.Pass();
     }
 }
