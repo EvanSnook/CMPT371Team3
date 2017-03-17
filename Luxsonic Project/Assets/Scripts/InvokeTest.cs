933e93f3f7cae07f8e27ccd141ca3b8ca1573333
IxLahiji <ixabat16@gmail.com>
2017-03-17 02:41:14 -0600

    Created external application to run DICOM conversion
    Can be executed outside of Unity, however experiences problems when
    executed within it.

---
+++ Luxsonic Project/Assets/Scripts/InvokeTest.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Diagnostics;
+using System.IO;
+
+public class InvokeTest : MonoBehaviour {
+
+
+
+	// Use this for initialization
+	void Start () {
+        string path = @"E:\Git Projects\CMPT371Team3\Luxsonic Project\Assets\Resources\DICOM";
+        Process newProcess = new Process();
+        newProcess.StartInfo.FileName = @"E:\Git Projects\CMPT371Team3\Converter\DICOMConverter.exe";
+        newProcess.StartInfo.Arguments = path;
+        newProcess.StartInfo.UseShellExecute = false;
+        newProcess.StartInfo.CreateNoWindow = true;
+        newProcess.EnableRaisingEvents = true;
+        newProcess.Start();
+        newProcess.WaitForExit();
+
+        UnityEngine.Debug.Log(newProcess.ExitCode);
+
+        var filePath = @"Assets\Resources\DICOM\temp\00000001";
+
+        if (File.Exists(filePath))
+        {
+            var fileData = File.ReadAllBytes(filePath);
+            var texture = new Texture2D(2, 2);
+            texture.LoadImage(fileData);
+            gameObject.GetComponent<GUITexture>().texture = texture;
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
