25590038f4eb68c377ef2dca06721df7fb56b353
J Hindman <idontwanttotellyoumyemail@goaway.com>
2017-03-17 19:59:50 -0600

    Added references to opposite hands to try and fix double-hand grasping bug.

--- Luxsonic Project/Assets/Scripts/TouchAndGrab.cs
+++ Luxsonic Project/Assets/Scripts/TouchAndGrab.cs
@@ -5,40 +5,72 @@ public class TouchAndGrab : MonoBehaviour
 {
     // the OVR controller
     public OVRInput.Controller controller;
-    // name of the axis for the grab trigger
-    [SerializeField]
-    private string grabTrigger;
-    // name of the axis for the index trigger
-    [SerializeField]
-    private string indexTrigger;
-    // the object we are touching/grabbing
-    public GameObject _object;
-    // the radius of the sphere for the raycast
-    public float _sphereRadius;
-    // This variable will be updated
-    public LayerMask _someMask;
-    // Indicates whether an object is being grabbed or not
-    private bool _isGrabbed;
 
-    private Transform objectsOldParent;
+    // name of the axis for the grab trigger
+	// Set in editor; below value is a default only
+    [SerializeField]
+    private string grabTrigger = "";
+    // name of the axis for the index trigger
+	// Set in editor; below value is a default only
+    [SerializeField]
+    private string indexTrigger = "";
+    
+
+
+	// Reference to the user's other hand
+	private TouchAndGrab oppositeHand = null;
+
+	// GameObject for the user's other hand
+	[SerializeField]
+	private GameObject oppositeHandObject = null;
+
+	// the object we are touching/grabbing
+    public GameObject _object = null;
+
+    // the radius of the sphere for the raycast
+	// Set in editor; below value is a default only
+	[SerializeField]
+    private float _sphereRadius = 0.1f;
+
+    // This variable will be updated
+	//**UPDATED TO WHAT?**
+	public LayerMask _someMask = (LayerMask)null;
+    // Indicates whether an object is being grabbed or not
+	private bool _isHolding = false;
+
+    private Transform objectsOldParent = null;
+
+	void Start(){
+		oppositeHand = oppositeHandObject.GetComponent<TouchAndGrab> ();
+	}
+
+	public bool IsHolding(){
+		return _isHolding;
+	}
+
+	public GameObject GetHeldObject(){
+		return _object;
+	}
 
     /// <summary>
     /// Update is called once per frame
     /// On Update() check to see if the 11th or 12th axis is pressed or not pressed
     /// Call function Grab() on pressed
-    /// Call function Drop() on not pressed
+    /// Call function Drop() on not pressed and object held
     /// Pre:: 
     /// Post:: 
     /// Return:: 
     /// </summary>
     void Update()
     {
-        // If the object is not being grabbed, grab the object, else drop the object
-        if (!_isGrabbed && (Input.GetAxis(grabTrigger) == 1))
+        // If we are not holding something and the grab trigger is pressed, grab
+		if (!_isHolding && ((int)Input.GetAxis(grabTrigger) == 1))
             Grab();
-        if (_isGrabbed && (Input.GetAxis(grabTrigger) < 1))
+		// If we are holding something and release the trigger, drop it
+		if (_isHolding && (Input.GetAxis(grabTrigger) < 1))
             Drop();
     }
+
     /// <summary>
     /// Make the object being grabbed a child of the controller (hand) whenever it is within the raycast
     /// of the controller
@@ -49,13 +81,13 @@ public class TouchAndGrab : MonoBehaviour
     /// </summary>
     void Grab()
     {
-        // Check to see if the object has been grabbed
-        _isGrabbed = true;
         // Create an array for the amount of objects grabbed
         RaycastHit[] _objectHits;
-        // Store the amount of objects that are gathered from the Sphere Cast
+        
+		// Store the amount of objects that are gathered from the Sphere Cast
         _objectHits = Physics.SphereCastAll(transform.position, _sphereRadius, transform.forward, 0F, _someMask);
-        // As long as there is an object that is grabbed track the position of the object.
+        
+		// Find the closes object from the SphereCast hits, if any exist
         if (_objectHits.Length > 0)
         {
             int _closestHit = 0;
@@ -65,14 +97,28 @@ public class TouchAndGrab : MonoBehaviour
                 if (_objectHits[i].distance < _objectHits[_closestHit].distance)
                     _closestHit = i;
             }
-            // make the object a child of the controller, updating the position of the object
-            _object = _objectHits[_closestHit].transform.gameObject;
-            _object.GetComponent<Rigidbody>().isKinematic = true;
-            this.objectsOldParent = _object.transform.parent;
-            _object.transform.position = transform.position;
-            _object.transform.parent = transform;
+
+			// Check to see if the other hand is holding something
+			// If it is, and it's the same as the closest raycast object, don't grab
+			if (oppositeHand.IsHolding ()) {
+				if (oppositeHand.GetHeldObject () != _objectHits [_closestHit]) {
+					AssignGrabbedObject (_objectHits [_closestHit].transform.gameObject);
+				}
+			} else {
+				AssignGrabbedObject (_objectHits[_closestHit].transform.gameObject);
+			}
         }
     }
+
+	void AssignGrabbedObject(GameObject closest){
+		_isHolding = true;
+		_object = closest;
+		_object.GetComponent<Rigidbody>().isKinematic = true;
+		this.objectsOldParent = _object.transform.parent;
+		_object.transform.position = transform.position;
+		_object.transform.parent = transform;
+	}
+
     /// <summary>
     /// Remove the object as a child of the controller 
     /// Pre:: an object is being grabbed
@@ -82,7 +128,8 @@ public class TouchAndGrab : MonoBehaviour
     void Drop()
     {
         //Change the boolean so the object is not grabbed anymore
-        _isGrabbed = false;
+		_isHolding = false;
+
         //If the object is attached change the transform to null so it keeps it's position
         if (_object != null)
         {
@@ -101,14 +148,14 @@ public class TouchAndGrab : MonoBehaviour
     /// </summary>
     private void OnTriggerEnter(Collider other)
     {
-        if ((other.tag == "MenuButton") && (Input.GetAxis(grabTrigger) == 1) && (Input.GetAxis(indexTrigger) < 1))
+		if ((other.tag == "MenuButton") && ((int)Input.GetAxis(grabTrigger) == 1) && (Input.GetAxis(indexTrigger) < 1))
         {
             if (!(other.gameObject.GetComponent<VRButton>().GetPressed()))
             {
                 other.gameObject.GetComponent<VRButton>().SetPressed(true);
             }
         }
-        else if ((other.tag == "Copy") && (Input.GetAxis(grabTrigger) == 1) && (Input.GetAxis(indexTrigger) < 1))
+		else if ((other.tag == "Copy") && ((int)Input.GetAxis(grabTrigger) == 1) && (Input.GetAxis(indexTrigger) < 1))
         {
             other.gameObject.SendMessage("Selected");
         }
