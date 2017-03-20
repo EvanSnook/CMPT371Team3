b90518a12f312e30fb26434e0f20ee6f15850302
IxLahiji <ixl164@usask.ca>
2017-03-19 22:33:33 -0600

    Fixed issue with test throwing Null Reference

--- Luxsonic Project/Assets/UnityTestTools/UnitTesting/Editor/DashboardTests.cs
+++ Luxsonic Project/Assets/UnityTestTools/UnitTesting/Editor/DashboardTests.cs
@@ -134,6 +134,19 @@ public class DashboardTests {
 		dash.dummyImages = new Texture2D[1];
 		dash.dummyImages.SetValue (new Texture2D(5,6), 0);
 
+        dash.buttonAttributes = new Dashboard.ButtonAttributes[] {
+			new Dashboard.ButtonAttributes("Load", new Vector3(0, 0.3f, 0)),
+			new Dashboard.ButtonAttributes("Quit", new Vector3(0, 0, 0)),
+			new Dashboard.ButtonAttributes("Minimize", new Vector3(0, -0.3f, 0)),
+			new Dashboard.ButtonAttributes("Contrast", new Vector3(-0.3f, 0.2f, 0)),
+			new Dashboard.ButtonAttributes("Rotate", new Vector3(0, 0.2f, 0)),
+			new Dashboard.ButtonAttributes("Zoom", new Vector3(0.3f, 0.2f, 0)),
+			new Dashboard.ButtonAttributes("Brightness", new Vector3(-0.3f, -0.2f, 0)),
+			new Dashboard.ButtonAttributes("Resize", new Vector3(0, -0.2f, 0)),
+			new Dashboard.ButtonAttributes("Filter", new Vector3(0.3f, -0.2f, 0)),
+			new Dashboard.ButtonAttributes("Close", new Vector3(0, -0.7f, 0))
+		};
+
 		//set up dashboard
 		dash.planePrefab = new GameObject();
 		dash.SetCopyButtons( new List<VRButton>());
