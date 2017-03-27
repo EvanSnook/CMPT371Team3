daeb68b6bec95676bc3a202df62c9e318be56c7b
mkp003 <mkp003@mail.usask.ca>
2017-03-26 20:50:34 -0600

    Adding the new converter file and updated filebrowser

---
+++ Luxsonic Project/Assets/Scripts/DICOMConverter.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Diagnostics;
+using System;
+
+public class DICOMConverter : MonoBehaviour {
+
+	private bool waitingForConversion;
+
+	public IEnumerator ExternalConverter(string type, string targetPath, string destinationPath)
+	{
+		Process newProcess = new Process(); // uncomment using system diagnostics to fix
+		newProcess.StartInfo.FileName = Application.dataPath + @"\DICOMConverter\DICOMConverter.exe";
+		newProcess.StartInfo.Arguments = type + " \"" + targetPath + "\" " + "\"" + destinationPath + "\"";
+		newProcess.StartInfo.UseShellExecute = false;
+		newProcess.StartInfo.CreateNoWindow = true;
+		newProcess.EnableRaisingEvents = true;
+		UnityEngine.Debug.Log("Test2");
+		waitingForConversion = true;
+		newProcess.Exited += new EventHandler(ProcessExited);
+
+		newProcess.Start();
+
+		while (waitingForConversion)
+		{
+			yield return null;
+		}
+	}
+
+	internal void ProcessExited(object sender, System.EventArgs e)
+	{
+		waitingForConversion = false;
+	}
+}
