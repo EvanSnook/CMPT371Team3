4d617508b1cd98719a2d2b052e93b5d2b6601823
kts135 <kts135@mail.usask.ca>
2017-04-06 09:01:25 -0600

    Actually fixed the tray tests

--- Luxsonic Project/Assets/UnityTestTools/UnitTesting/Editor/TrayTests.cs
+++ Luxsonic Project/Assets/UnityTestTools/UnitTesting/Editor/TrayTests.cs
@@ -129,6 +129,86 @@ public class TrayTests : MonoBehaviour
     [Test]
     public void TestHighlightTray()
     {
-        Assert.Fail();
+        GameObject trayManager = new GameObject();
+        trayManager.AddComponent<Display>();
+
+        GameObject trayObject = new GameObject();
+        trayObject.AddComponent<Tray>();
+
+        GameObject thumbObject = new GameObject();
+        thumbObject.AddComponent<Thumbnail>();
+        thumbObject.AddComponent<SpriteRenderer>();
+
+        GameObject thumbOutline = new GameObject();
+        thumbOutline.AddComponent<SpriteRenderer>();
+        thumbOutline.transform.SetParent(thumbObject.transform);
+
+        GameObject buttonPrefab = new GameObject();
+        buttonPrefab.AddComponent<VRButton>();
+
+        GameObject textObject = new GameObject();
+        textObject.AddComponent<TextMesh>();
+        textObject.transform.SetParent(buttonPrefab.transform);
+
+        trayObject.GetComponent<Tray>().thumbnailPrefab = thumbObject;
+        trayObject.GetComponent<Tray>().buttonPrefab = buttonPrefab;
+        trayObject.GetComponent<Tray>().manager = trayManager.GetComponent<Display>();
+        trayObject.GetComponent<Tray>().trayNumColumns = 3;
+
+        Texture2D tex1 = new Texture2D(100, 100);
+        Texture2D tex2 = new Texture2D(100, 100);
+        Texture2D tex3 = new Texture2D(100, 100);
+        Texture2D tex4 = new Texture2D(100, 100);
+        Texture2D tex5 = new Texture2D(100, 100);
+        Texture2D tex6 = new Texture2D(100, 100);
+        Texture2D tex7 = new Texture2D(100, 100);
+        Texture2D tex8 = new Texture2D(100, 100);
+        Texture2D tex9 = new Texture2D(100, 100);
+        Texture2D tex10 = new Texture2D(100, 100);
+
+        List<Texture2D> textures = new List<Texture2D>();
+
+        textures.Add(tex1);
+        textures.Add(tex2);
+        textures.Add(tex3);
+        textures.Add(tex4);
+        textures.Add(tex5);
+        textures.Add(tex6);
+        textures.Add(tex7);
+        textures.Add(tex8);
+        textures.Add(tex9);
+        textures.Add(tex10);
+
+        trayObject.GetComponent<Tray>().Setup(tex1);
+        trayObject.GetComponent<Tray>().UpdateTray(tex1);
+        trayObject.GetComponent<Tray>().UpdateTray(tex2);
+        trayObject.GetComponent<Tray>().UpdateTray(tex3);
+        trayObject.GetComponent<Tray>().UpdateTray(tex4);
+        trayObject.GetComponent<Tray>().UpdateTray(tex5);
+        trayObject.GetComponent<Tray>().UpdateTray(tex6);
+        trayObject.GetComponent<Tray>().UpdateTray(tex7);
+        trayObject.GetComponent<Tray>().UpdateTray(tex8);
+        trayObject.GetComponent<Tray>().UpdateTray(tex9);
+        trayObject.GetComponent<Tray>().UpdateTray(tex10);
+
+        Texture2D[] texturesToHighlight = new Texture2D[3];
+        texturesToHighlight[0] = tex1;
+        texturesToHighlight[1] = tex2;
+        texturesToHighlight[2] = tex3;
+
+
+        trayObject.GetComponent<Tray>().HighlightTray(texturesToHighlight);
+
+        foreach (GameObject thumb in trayObject.GetComponent<Tray>().GetThumbnails())
+        {
+            if (thumb.GetComponent<Thumbnail>().image == tex1 || thumb.GetComponent<Thumbnail>().image == tex2 || thumb.GetComponent<Thumbnail>().image == tex3)
+            {
+                Assert.IsTrue(thumb.transform.GetChild(0).transform.gameObject.activeSelf);
+            }
+            else
+            {
+                Assert.IsFalse(thumb.transform.GetChild(0).transform.gameObject.activeSelf);
+            }
+        }
     }
 }
